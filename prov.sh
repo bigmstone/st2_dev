3b9492100e1406dd798c2ad2d7a3945012745934
bigmstone <matthew99@gmail.com>
2017-03-27 17:43:50 -0500

    Adding mistral reqs

--- prov.sh
+++ prov.sh
@@ -1,4 +1,6 @@
 apt-get update
-apt-get -y install make python-pip libffi-dev mongodb postgresql libssl-dev rabbitmq-server
+apt-get -y install make python-pip libffi-dev mongodb postgresql libssl-dev \
+    rabbitmq-server python-dev python-setuptools libxslt1-dev libxml2-dev \
+    libyaml-dev
 pip install --upgrade pip
 pip install virtualenv
